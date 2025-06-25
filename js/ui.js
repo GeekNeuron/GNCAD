5493fbaab2138ae0f8fdf865d3160def2bd1731f
Geek Neuron <geekneuron@gmail.com>
2025-06-25 23:48:51 +0330

    Create ui.js

---
+++ js/ui.js
@@ -0,0 +1,4 @@
+// This module handles all UI interactions outside of the canvas
+export function initUI(canvas) { /* ... code for all button listeners, panels, modals ... */ }
+export function updatePropertiesPanel(obj) { /* ... code ... */ }
+export function setMode(mode) { /* ... code ... */ }
