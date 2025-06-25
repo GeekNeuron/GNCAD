65a361e34e80b8cf919efd734a0d4e9650df79e5
Geek Neuron <geekneuron@gmail.com>
2025-06-25 23:52:35 +0330

    Create history.js

---
+++ js/history.js
@@ -0,0 +1,4 @@
+// Undo/Redo functionality
+export function saveState(canvas) { /* ... code ... */ }
+export function undo(canvas) { /* ... code ... */ }
+export function redo(canvas) { /* ... code ... */ }
