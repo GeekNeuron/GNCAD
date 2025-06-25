fb32d049bdc27169363b98407ae444ed20166d8f
Geek Neuron <geekneuron@gmail.com>
2025-06-25 23:47:42 +0330

    Create symbols.js

---
+++ js/symbols.js
@@ -0,0 +1,6 @@
+// This module contains functions to create architectural symbols
+// It needs canvas and config to be passed or imported
+// For simplicity, we'll pass canvas as an argument.
+export function createDoorSymbol(fabric, canvas, x, y, angle = 0, width = 40 * 0.8) { /* ... code ... */ }
+export function createWindowSymbol(fabric, canvas, x, y, angle = 0, width = 40 * 1.2) { /* ... code ... */ }
+// ... and so on for all symbol creation functions.
