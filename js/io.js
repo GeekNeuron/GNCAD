116ec1d87a75d11c295baaafe9370ef350a9a18f
Geek Neuron <geekneuron@gmail.com>
2025-06-25 23:51:24 +0330

    Create io.js

---
+++ js/io.js
@@ -0,0 +1,4 @@
+// Import/Export fanctionality
+export function saveProject(canvas) { /* ... code ... */ }
+export function loadProject(canvas, file) { /* ... code ... */ }
+export function importDxf(canvas, fileContent) { /* ... code using dxf-parser ... */ }
