ad99339a967a4698d767397f074fe359aae4ced0
Geek Neuron <geekneuron@gmail.com>
2025-06-25 23:50:01 +0330

    Create canvasSetup.js

---
+++ js/canvasSetup.js
@@ -0,0 +1,4 @@
+// This module sets up the canvas, grid, rulers, and navigation
+export function setupCanvas(fabric) { /* ... code to init fabric.Canvas ... */ }
+export function drawGrid(canvas) { /* ... code for pattern grid ... */ }
+export function setupNavigation(canvas) { /* ... code for zoom and pan ... */ }
