12fe59b1518cb6f0e57f2023073358f5f9856cf5
Geek Neuron <geekneuron@gmail.com>
2025-06-26 08:25:02 +0330

    Delete js/canvasSetup.js

--- js/canvasSetup.js
+++
@@ -1,4 +0,0 @@
-// This module sets up the canvas, grid, rulers, and navigation
-export function setupCanvas(fabric) { /* ... code to init fabric.Canvas ... */ }
-export function drawGrid(canvas) { /* ... code for pattern grid ... */ }
-export function setupNavigation(canvas) { /* ... code for zoom and pan ... */ }
