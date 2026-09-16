83fb78f79999df66650b125a5a2022c9fdbe6dbb
Geek Neuron <geekneuron@gmail.com>
2026-09-16 08:43:51 +0330

    Delete js/ui.js

--- js/ui.js
+++
@@ -1,4 +0,0 @@
-// This module handles all UI interactions outside of the canvas
-export function initUI(canvas) { /* ... code for all button listeners, panels, modals ... */ }
-export function updatePropertiesPanel(obj) { /* ... code ... */ }
-export function setMode(mode) { /* ... code ... */ }
